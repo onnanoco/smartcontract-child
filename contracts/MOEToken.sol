137ef4033b8106383f3f7fa50f73620fbaa74671
onnanoco <moe@onnano.co>
2022-01-21 17:05:09 +0900

    update

--- contracts/MOEToken.sol
+++ contracts/MOEToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.6.0;
+pragma solidity 0.6.6;
 
 import "./Library.sol";
 import "@maticnetwork/pos-portal/contracts/child/ChildToken/UpgradeableChildERC20/UChildERC20.sol";
@@ -19,9 +19,9 @@ import "@maticnetwork/pos-portal/contracts/child/ChildToken/UpgradeableChildERC2
 //
 contract MOEToken is UchildERC20 {
 
-    string public constant NAME = "MOE Token";
-    string public constant SYMBOL = "MOE";
-    uint8 public constant DECIMALS = 18;
+    //string public constant NAME = "MOE Token";
+    //string public constant SYMBOL = "MOE";
+    //uint8 public constant DECIMALS = 18;
 
     mapping(uint256 => Lib.Onnanoco) public onnanocos;
     mapping(uint256 => Lib.Round) public rounds;
@@ -36,10 +36,6 @@ contract MOEToken is UchildERC20 {
 
     bytes32 public constant DEPOSITOR_ROLE = keccak256("DEPOSITOR_ROLE"); // Polygon mapping
 
-    // Add onnanoco
-    // name: name of the character
-    // hash, hashFunction, hashSize: multihash
-    // amount: defense amount
     function addOnnanoco(string memory name, bytes32 hash, bytes2 hashFunction, uint8 hashSize, uint256 amount) public {
   
         _burn(_msgSender(), amount);
@@ -48,15 +44,27 @@ contract MOEToken is UchildERC20 {
         onnanocos[totalOnnanocos] = Lib.Onnanoco(name, _msgSender(), Lib.Multihash(hash, hashFunction, hashSize), Lib.Status.NORMAL, block.timestamp, 0, totalRounds);
 
         // setup round
-        rounds[totalRounds] = Lib.Round(totalOnnanocos, amount, 0, block.timestamp);
+        rounds[totalRounds] = Lib.Round(totalOnnanocos, amount, 0, 0);
 
         // vote for defense
-        Lib.Vote memory vote = Lib.Vote(totalOnnanocos, _msgSender(), amount, block.timestamp);
+        Lib.Vote vote = Lib.Vote(totalOnnanocos, _msgSender(), amount, block.timestamp);
         defenseVotes[totalRounds].push(vote);
 
         // increase onnanoco index
         totalOnnanocos.add(1);
         totalRounds.add(1);
+    }
+
+    // Get minimum attack amount
+    // id : id of the character
+    function getMinimumAttackAmount(uint256 id) public view returns (uint256 amount) {
+
+        uint256 roundId = onnanocos[id].roundId;
+        Lib.Round memory round = rounds[roundId];
+
+        uint256 minimumAmount = (round.totalDefenseAmount.add(round.totalAttackAmount).mul(2)).sub(round.totalAttackAmount.mul(3));
+
+        return minimumAmount;
     }
 
     // Vote for attack
@@ -67,28 +75,38 @@ contract MOEToken is UchildERC20 {
         require(amount > 0, 'Cannot vote with 0 MOE');
         require(onnanocos[id].status != Lib.Status.DEPRECATED, 'Deprecated');
         
-        // [TODO] check experation period
+        uint256 minimumAmount = getMinimumAttackAmount(id);
 
-        uint256 roundId = onnanocos[id].roundId;
-
-        Lib.Round memory round = rounds[roundId];
-
-        uint256 duration = block.timestamp.sub(round.timestamp);
-
-        //require(duration > 60 * 60 * 7, 'Minimum duration is 7 days'); // deploy
-        require(duration > 60 * 60 * 1, 'Minimum duration is 1 day'); // dev
-        
-        require(amount >= (round.totalDefenseAmount.add(round.totalAttackAmount).mul(2)).sub(round.totalAttackAmount.mul(3)), 'Not enough MOE');
+        require(amount >= minimumAmount, 'Less then MIN attack amount');
 
         _burn(_msgSender(), amount);
 
         // 1st attack
         if (onnanocos[id].status == Lib.Status.NORMAL) {
+
+            uint256 duration = block.timestamp.sub(round.timestamp);
+            //require(duration < 60 * 60 * 24 * 7, 'Dispute window is 7 days'); // deploy
+            require(duration < 60 * 60, 'Dispute window is 1 hour'); // dev
+
             onnanocos[id].status = Lib.Status.IN_DISPUTE;
+        } else {
+            rounds[roundId].timestamp = block.timestamp;
         }
 
         attackVotes[roundId].push(Lib.Vote(id, _msgSender(), amount, block.timestamp));
-        rounds[roundId].totalAttackAmount += amount;
+        rounds[roundId].totalAttackAmount.add(amount);
+    }
+
+    // Get minimum defense amount
+    // id : id of the character
+    function getMinimumDefenseAmount(uint256 id) public view returns (uint256 amount) {
+
+        uint256 roundId = onnanocos[id].roundId;
+        Lib.Round memory round = rounds[roundId];
+
+        uint256 minimumAmount = ((round.totalDefenseAmount.add(round.totalAttackAmount)).mul(2)).sub(round.totalDefenseAmount.mul(3));
+
+        return minimumAmount;
     }
 
     // Vote for defense
@@ -99,19 +117,15 @@ contract MOEToken is UchildERC20 {
         require(amount > 0, 'Cannot vote with 0 MOE');
         require(onnanocos[id].status == Lib.Status.IN_DISPUTE, 'Round is not in dispute');
 
-        // [TODO] check experation period
-
-        uint256 roundId = onnanocos[id].roundId;
-
-        Lib.Round memory round = rounds[roundId];
-
         uint256 duration = block.timestamp.sub(round.timestamp);
 
-        //require(duration > 60 * 60 * 7, 'Minimum duration is 7 days'); // deploy
-        require(duration > 60 * 60 * 1, 'Minimum duration is 1 day'); // dev
+        //require(duration > 60 * 60 * 24 * 7, 'Dispute window is 7 days'); // deploy
+        require(duration > 60 * 60 * 1, 'Dispute window is 1 hour'); // dev
 
-        require(amount >= (round.totalDefenseAmount.add(round.totalAttackAmount).mul(2)).sub(round.totalDefenseAmount.mul(3)), 'Not enough MOE');
+        uint256 minimumAmount = getMinimumDefenseAmount(id);
 
+        require(amount >= minimumAmount, 'Less then MIN defense amount');
+        
         _burn(_msgSender(), amount);
         defenseVotes[roundId].push(Lib.Vote(id, _msgSender(), amount, block.timestamp));
 
@@ -119,25 +133,36 @@ contract MOEToken is UchildERC20 {
     }
 
     // clearRound
+    // roundId : ID for the round
     function clearRound(uint256 roundId) public {
 
         Lib.Round memory round = rounds[roundId];
-        Lib.Onnanoco memory onnanoco = onnanocos[round.onnanocoId];
+        Lib.Onnanoco storage onnanoco = onnanocos[round.onnanocoId];
 
         require(onnanoco.roundId == roundId, 'Invalid round id');
 
         uint256 duration = block.timestamp.sub(round.timestamp);
 
-        //require(duration > 60 * 60 * 7, 'Minimum duration is 7 days'); // deploy
-        require(duration > 60 * 60 * 1, 'Minimum duration is 1 day'); // dev
+        //require(duration > 60 * 60 * 24 * 7, 'Dispute window is not over'); // deploy
+        require(duration > 60 * 60 * 1, 'Dispute window is not over'); // dev
 
         if (round.totalAttackAmount > round.totalDefenseAmount) { // Attackers win
             _mint(_msgSender(), round.totalAttackAmount.div(50));
-            onnanocos[round.onnanocoId].status = Lib.Status.DEPRECATED;
+            onnanoco.status = Lib.Status.DEPRECATED;
 
         } else if(round.totalAttackAmount < round.totalDefenseAmount) { // Attackeres lose
             _mint(_msgSender(), round.totalDefenseAmount.div(50));
-            onnanocos[round.onnanocoId].status = Lib.Status.NORMAL;
+            onnanoco.status = Lib.Status.NORMAL;
+
+            // new round
+            rounds[totalRounds] = Lib.Round(round.onnanocoId, 0, 0, 0);
+
+            Lib.Vote memory vote = defenseVotes[roundId][0];
+
+            Lib.Vote newVote = Lib.Vote(round.onnanocoId, vote.voter, vote.amount, block.timestamp);
+            defenseVotes[totalRounds].push(newVote);
+
+            totalRounds.add(1);
         }
         
     }
@@ -151,8 +176,8 @@ contract MOEToken is UchildERC20 {
 
         uint256 duration = block.timestamp.sub(round.timestamp);
 
-        //require(duration > 60 * 60 * 24 * 7, 'Minimum duration is 7 days'); // deploy
-        require(duration > 60 * 60 * 1, 'Minimum duration is 1 day'); // dev
+        //require(duration > 60 * 60 * 24 * 7, 'Dispute window is 7 days'); // deploy
+        require(duration > 60 * 60 * 1, 'Dispute window is 1 hour'); // dev
 
         uint256 totalAmount = round.totalAttackAmount.add(round.totalDefenseAmount);
 
@@ -175,12 +200,13 @@ contract MOEToken is UchildERC20 {
 
         uint256 duration = block.timestamp.sub(round.timestamp);
 
-        //require(duration > 60 * 60 * 24 * 7, 'Minimum duration is 7 days'); // deploy
-        require(duration > 60 * 60 * 1, 'Minimum duration is 1 days'); // dev
+        //require(duration > 60 * 60 * 24 * 7, 'Dispute window is 7 days'); // deploy
+        require(duration > 60 * 60 * 1, 'Dispute window is 1 hour'); // dev
 
         uint256 totalAmount = round.totalAttackAmount.add(round.totalDefenseAmount);
 
-        if (round.totalAttackAmount < round.totalDefenseAmount) {
+        if (round.totalAttackAmount < round.totalDefenseAmount && voteId > 0) {
+
             _mint(_msgSender(), totalAmount.mul(vote.amount).div(round.totalDefenseAmount));
             defenseVotes[roundId][voteId].voter = address(0);
 
@@ -211,9 +237,9 @@ contract MOEToken is UchildERC20 {
         uint256 duration = block.timestamp - stakeInfo.timestamp;
 
         //require(duration > 60 * 60 * 24 * 100, 'Minimum duration is 100 days'); // Deploy
-        require(duration > 60 * 60 * 1, 'Minimum duration is 100 days'); // Test
+        require(duration > 60 * 60 * 1, 'Minimum duration is 1 hour'); // Test
 
-        uint256 bonus = stakeInfo.amount.mul(duration * 5).div(60 * 60 * 24 * 100 * 100);
+        uint256 bonus = stakeInfo.amount.mul(duration).div(60 * 60 * 24 * 100 * 20);
 
         if (onnanocos[stakeInfo.id].status == Lib.Status.DEPRECATED) {
             bonus = 0;
