0e1d1d39e5a85cea853f0444f1d7bcea7e3baa3e
onnanoco <moe@onnano.co>
2022-01-24 09:30:08 +0900

    initial commit

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -10,5 +10,7 @@ module.exports = async function(deployer) {
 */
 
 module.exports = async function(deployer) {
-    await deployProxy(MOEToken, ['MOE Token', 'MOE', '18', '0xb5505a6d998549090530911180f38aC5130101c6'], { deployer, initializer: 'initialize' });
+    await deployProxy(MOEToken, ['MOE Token', 'MOE', '0xb5505a6d998549090530911180f38aC5130101c6'], { deployer, initializer: 'initialize' }); // for Mumbai testnet
+
+    //await deployProxy(MOEToken, ['MOE Token', 'MOE', '0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa'], { deployer, initializer: 'initialize' }); // for mainnet
 };
